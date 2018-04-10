d49878f76052f6d54c0957821d8c4fceb90693ea
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-04-09 19:26:18 -0700

    I have no idea

--- letter.js
+++ letter.js
@@ -1,6 +1,22 @@
 function Letter(character, guessed) {
-    this.character = ;
-    this.guessed = ;
-    this.printCharacter = function () {};
-    this.checkCharacter = function () {};
+    this.character = character;
+    this.guessed = guessed;
 }
+
+Letter.prototype.printCharacter = function () {
+    if (this.guessed === true) {
+        console.log(this.character);
+        return this.character;
+    } else {
+        console.log("-");
+        return "-";
+    }
+};
+Letter.prototype.checkCharacter = function (guess) {
+    if (guess === this.character) {
+        this.guessed = true;
+    }
+};
+
+var newLetter = new Letter("a", true);
+console.log(newLetter.printCharacter());
--- word.js
+++ word.js
@@ -1,5 +1,8 @@
-function Word(fullWord) {
-    this.fullWord = ;
-    this.displayWord = function () {};
-    this.check = function () {};
+function Word(letters) {
+    this.letters = letters;
 }
+
+Word.displayWord = function () {
+
+};
+Word.check = function () {};
